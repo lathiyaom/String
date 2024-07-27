acb14163bbc4f20b2d2b64de13489a01eeebc15e
om lathiya <lathiya52725@gmail.com>
2024-07-27 18:09:34 +0530

    this is leetcode no 151

--- src/LeetCode151.java
+++ src/LeetCode151.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class LeetCode151 {
     public static void main(String[] args) {
+        String str="a good   example";
+       String ans=reverseWords(str);
+        System.out.println(ans);
+    }
 
+    private static String reverseWords(String str) {
+        //this is
+            str=str.trim();
+        List<String> stringList= Arrays.asList(str.split("\\s+"));
+        Collections.reverse(stringList);
+        return String.join(" ",stringList);
     }
 }
