1507b648b33661c9ca3ab477666162b70690ab12
Om Lathiya <lathiya52725@gmail.com>
2026-01-27 08:04:28 +0530

    Solve the bug and logical error into next permutations leetcode 31

---
+++ src/Leetcode/Next_Permutation_31.java
@@ -0,0 +1,39 @@
+package Leetcode;
+
+
+public class Next_Permutation_31 {
+    static void main() {
+        nextPermutation(new int[]{1, 1});
+    }
+
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        revers(nums, i + 1);
+    }
+
+    public static void revers(int[] nums, int start) {
+        int i = start;
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+}
